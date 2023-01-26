8d8b866b4fea87c9687e172d5aeb579eb08898a5
em-eight <m8em.eightm8@gmail.com>
2023-01-27 01:47:08 +0200

    Fix looping instruction check

--- include/aipg/aipg.hpp
+++ include/aipg/aipg.hpp
@@ -9,7 +9,7 @@ namespace aipg {
 struct Context {
   std::unordered_map<uint32_t, uint32_t> gprs;
   std::unordered_map<uint32_t, uint32_t> fprs;
-  std::unordered_map<uint32_t, uint32_t> imms;
+  std::unordered_map<uint32_t, int32_t> imms;
 
   /// @brief The index of each instruction that matched with the idiom's assembly lines from the starting instruction
   std::vector<uint32_t> matchInsIdxs;
